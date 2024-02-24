3b6d92266862e8cb866d29cf738228ef15c261de
abduazim10 <abduazimit@gmail.com>
2024-02-24 15:14:46 +0500

    tugai

---
+++ main.js
@@ -0,0 +1,36 @@
+// console.time()
+
+// console.log('external')
+
+// console.warn('ogohlantirish')
+// console.error('xatolik')
+// console.info('habar berish')
+
+// console.timeEnd()
+
+// var son = 423233;
+// var son = 2384392;
+
+// let raqam = 232943222;
+
+// const pi = 3.14
+
+// {
+//     var a = 23;
+// }
+
+// console.log(a)
+// console.log(son)
+// console.log(raqam)
+// console.log(pi)!
+var ism = prompt('ism kiriting')
+let fam = prompt('familiya kiriting')
+let yosh = prompt('yoshni kiriting')
+let fan = prompt('yoqtirgan fanni kiriting')
+let tuman = prompt('Tumaningizni kiriting')
+
+console.log('ism:' + ism);
+console.log('fam:' + fam);
+console.log('yosh:' + yosh);
+console.log('fan:' + fan);
+console.log('tuman:' + tuman);
